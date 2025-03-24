8f51bcf8122038932e4ef86d6ab777f656b042d3
sisu2468 <danielcoder1022@gmail.com>
2025-03-24 21:59:58 +0900

    fix: header navItem

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
 'use client';
 
+import { navItems } from '@/constant/navItems';
 import { ChevronDownIcon, HamburgerIcon } from '@chakra-ui/icons';
 import {
   Box,
   Button,
-  Container,
   Flex,
   HStack,
   Icon,
@@ -13,57 +13,13 @@ import {
   MenuButton,
   MenuItem,
   MenuList,
-  Text,
+  Text
 } from '@chakra-ui/react';
 import gsap from 'gsap';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { useEffect, useRef, useState } from 'react';
-
-interface NavItem {
-  title: string;
-  path: string;
-  description: string;
-}
-
-const navItems: NavItem[] = [
-  {
-    title: 'トップ',
-    path: '/',
-    description: 'サービス概要や導入メリットを紹介',
-  },
-  {
-    title: 'サービス',
-    path: '/services',
-    description: 'ERPの各機能一覧',
-  },
-  {
-    title: 'サインタラボ',
-    path: '/signta-lab',
-    description: 'webシステムの開発',
-  },
-  {
-    title: 'サインタ・コネクト',
-    path: '/signta-connect',
-    description: '専門家マッチングプラットフォーム',
-  },
-  {
-    title: 'プラン・料金',
-    path: '/pricing',
-    description: '料金体系と導入プラン',
-  },
-  {
-    title: 'お役立ち情報',
-    path: '/resources',
-    description: '導入ガイド・活用事例・業務効率化のコツ',
-  },
-  {
-    title: 'お問い合わせ・無料体験',
-    path: '/contact',
-    description: '相談・無料トライアル申し込み',
-  },
-];
+import { useRef, useState } from 'react';
 
 export default function Header() {
   const navRef = useRef<HTMLElement>(null);
