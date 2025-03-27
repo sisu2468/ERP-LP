8eb565b4f20d3a130263ec78cba8476e344972f6
sisu2468 <danielcoder1022@gmail.com>
2025-03-27 18:27:34 +0900

    feat: career process

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -1,12 +1,14 @@
 import { Container } from "@chakra-ui/react";
 import CareerHero from "@/components/pages/career/CareerHero";
 import CompanyCulture from "@/components/pages/career/culture/CompanyCulture";
+import CareerProcess from "@/components/pages/career/process/CareeerProcess";
 
 export default function Career() {
     return (
         <Container maxW="full" p={0}>
             <CareerHero />
             <CompanyCulture />
+            <CareerProcess />
         </Container>
     )
 }
