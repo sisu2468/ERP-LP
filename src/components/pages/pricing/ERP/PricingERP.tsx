93a8f484db3046576679801846fc49ae54906819
sisu2468 <danielcoder1022@gmail.com>
2025-03-28 18:47:48 +0900

    remove deduct component

--- src/components/pages/pricing/ERP/PricingERP.tsx
+++ src/components/pages/pricing/ERP/PricingERP.tsx
@@ -69,8 +69,6 @@ export default function PricingERP() {
 
                     <ModulePricing />
 
-                    <ContractDiscounts />
-
                     <PricingFeatures />
 
                     <PricingCalculator />
