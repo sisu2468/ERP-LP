e6b166a5dbd540c38133b407332a47d0438e1d55
sisu2468 <danielcoder1022@gmail.com>
2025-04-01 22:09:19 +0900

    feat: OGP

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,53 +1,34 @@
-'use client'
-
-import CTASection from "@/components/pages/home/CTASection";
-import FAQ from "@/components/pages/home/faq/FAQ";
-import FeatureDetailsSection from "@/components/pages/home/FeatureDetailsSection";
-import FlowChart from "@/components/pages/home/flowchart/FlowChart";
-import Hero from "@/components/pages/home/Hero";
-import ResourcesSection from "@/components/pages/home/ResourcesSection";
-import ServiceIntroSection from "@/components/pages/home/ServiceIntroSection";
+import HomePage from "@/components/pages/home/HomePage";
 import { Box } from "@chakra-ui/react";
 
-import gsap from "gsap";
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import type { Metadata, Viewport } from "next";
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+};
 
-import { useEffect, useRef } from "react";
-
-gsap.registerPlugin(ScrollTrigger);
+export const metadata: Metadata = {
+  title: "【株式会社サインタ】サインタ・コアにはどんな機能が入ってる？",
+  description: "サインタERPの基本機能「サインタ・コア」には、ビジネスをスムーズに運営するための必須ツールが揃っています。",
+  metadataBase: new URL('https://sainta.co.jp/home'),
+  icons: {
+    icon: "/favicons/favicon.ico",
+  },
+  openGraph: {
+    images: [{
+      url: '/favicons/favicon-96x96.png',
+      width: 96,
+      height: 96,
+      alt: 'サインタ・コア Preview Image',
+    }],
+  },
+};
 
 export default function Home() {
-  const sectionsRef = useRef([]);
-
-  useEffect(() => {
-    sectionsRef.current.forEach((section) => {
-      if (!section) return;
-
-      gsap.fromTo(
-        section,
-        { opacity: 0, y: 50 },
-        {
-          opacity: 1, y: 0, duration: 1.2, ease: "circ.out",
-          scrollTrigger: {
-            trigger: section,
-            start: "top 72.5%",
-            toggleActions: "play none none none",
-            once: true
-          }
-        }
-      );
-    });
-  }, []);
 
   return (
     <Box>
-      <Hero />
-      <ServiceIntroSection />
-      <FeatureDetailsSection />
-      <ResourcesSection />
-      <FlowChart />
-      <CTASection />
-      <FAQ />
+      <HomePage />
     </Box>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,29 @@ import ScrollButtons from "@/components/common/ScrollButtons";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { Box } from "@chakra-ui/react";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import { Providers } from "./providers";
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+};
 
 export const metadata: Metadata = {
   title: "サインタ・コアにはどんな機能が入ってる？",
   description: "サインタERPの基本機能「サインタ・コア」には、ビジネスをスムーズに運営するための必須ツールが揃っています。",
-  viewport: "width=device-width, initial-scale=1",
+  metadataBase: new URL('https://sainta.co.jp'),
   icons: {
     icon: "/favicons/favicon.ico",
+  },
+  openGraph: {
+    images: [{
+      url: '/favicons/favicon-96x96.png',
+      width: 96,
+      height: 96,
+      alt: 'サインタ・コア Preview Image',
+    }],
   },
 };
 
